adadb5bece9cf95248356ccbf9ab8de910dc5258
Yasmin Hansen <yaa-h@hotmail.com>
2020-09-15 15:02:22 -0300

    Ajuste no formulário

--- src/App.js
+++ src/App.js
@@ -5,11 +5,6 @@ import ButtonC from "./components/ButtonC";
 import ButtonSC from "./components/ButtonSC";
 
 import Form from 'react-bootstrap/Form';
-import FormControl from 'react-bootstrap/FormControl';
-import FormGroup from 'react-bootstrap/FormGroup';
-import FormLabel from 'react-bootstrap/FormLabel';
-import FormText from 'react-bootstrap/FormText';
-import FormCheck from 'react-bootstrap/FormCheck';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
@@ -26,21 +21,21 @@ class App extends Component {
 
         <h3>Exemplo 1</h3>
         <Form>
-          <FormGroup controlId="formBasicEmail">
-            <FormLabel>Email address</FormLabel>
-            <FormControl type="email" placeholder="Enter email"/>
-            <FormText className="text-muted">
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email"/>
+            <Form.Text className="text-muted">
               We'll never share your email with anyone else.
-            </FormText>
-          </FormGroup>
+            </Form.Text>
+          </Form.Group>
 
-          <FormGroup controlId="formBasicPassword">
-            <FormLabel>Password</FormLabel>
-            <FormControl type="password" placeholder="Password" />
-          </FormGroup>
-          <FormGroup controlId="formBasicCheckbox">
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group controlId="formBasicCheckbox">
             <Form.Check type="checkbox" label="Check me out" />
-          </FormGroup>
+          </Form.Group>
           <Button variant="primary" type="submit">
             Submit
           </Button>
@@ -48,23 +43,23 @@ class App extends Component {
 
         <hr></hr>
         <h3>Exemplo 2</h3>
-        <FormGroup>
-          <FormControl size="lg" type="text" placeholder="Large text" />
+        <Form.Group>
+          <Form.Control size="lg" type="text" placeholder="Large text" />
           <br />
-          <FormControl type="text" placeholder="Normal text" />
+          <Form.Control type="text" placeholder="Normal text" />
           <br />
-          <FormControl size="sm" type="text" placeholder="Small text" />
-        </FormGroup>
+          <Form.Control size="sm" type="text" placeholder="Small text" />
+        </Form.Group>
 
         <hr></hr>
         <h3>Exemplo 3</h3>
         <Form>
           <Row>
             <Col>
-              <FormControl placeholder="First name" />
+              <Form.Control placeholder="First name" />
             </Col>
             <Col>
-              <FormControl placeholder="Last name" />
+              <Form.Control placeholder="Last name" />
             </Col>
           </Row>
         </Form>
@@ -74,19 +69,19 @@ class App extends Component {
         <Form>
           <Row>
             <Col>
-              <FormControl placeholder="First name" />
+              <Form.Control placeholder="First name" />
             </Col>
             <Col>
-              <FormControl placeholder="Last name" />
+              <Form.Control placeholder="Last name" />
             </Col>
           </Row>
         </Form>
         <Form>
           {['checkbox', 'radio'].map((type) => (
             <div key={`inline-${type}`} className="mb-3">
-              <FormCheck inline label="1" type={type} id={`inline-${type}-1`} />
-              <FormCheck inline label="2" type={type} id={`inline-${type}-2`} />
-              <FormCheck
+              <Form.Check inline label="1" type={type} id={`inline-${type}-1`} />
+              <Form.Check inline label="2" type={type} id={`inline-${type}-2`} />
+              <Form.Check
                 inline
                 disabled
                 label="3 (disabled)"
